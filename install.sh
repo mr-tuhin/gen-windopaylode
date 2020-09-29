e2f0eaef000e052eaa64a6a6d3fb1e0dfc4818dc
Tuhinsubhra ghara <70248518+tuhinsghara@users.noreply.github.com>
2020-09-29 07:06:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 hidden='\e[8m'
 sudo pwd
 sudo mv -v .index.sh /usr/local/bin/genwindopaylode
-chmod +x /usr/local/bin/genpaylode
+chmod +x /usr/local/bin/genwindopaylode
 rm -rf install.sh
 cd ..
 exit
